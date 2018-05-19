735af690fecbdee8e76fe900ff4e19015538212b
Haagimus <Haagimus@gmail.com>
2018-05-19 00:44:51 -0500

    initial commit

--- Calculator Automation App/WebpageHelpers.cs
+++ Calculator Automation App/WebpageHelpers.cs
@@ -4,11 +4,9 @@ using System.Collections.Generic;
 
 namespace Automation_Example_App
 {
-    public class WebpageHelpers
+    public static class WebpageHelpers
     {
-        private static IWebDriver driver;
-
-        public static IWebDriver Driver { get => driver; set => driver = value; }
+        public static IWebDriver Driver { get; set; }
 
         // Open the calculator webpage and return the driver object so it can be referenced in the main form
         public static IWebDriver OpenWebpage(string hyperlink)
@@ -33,13 +31,11 @@ namespace Automation_Example_App
         /// <returns>The web element that matches the search criteria or returns nothing.</returns>
         public static IWebElement GetElementByClass(IWebDriver driver, string searchClass, string searchText)
         {
-            var results = driver.FindElements(By.ClassName(searchClass));
-
-            foreach (var r in results)
+            foreach (var item in driver.FindElements(By.ClassName(searchClass)))
             {
-                if (r.Text == searchText)
+                if (item.Text == searchText)
                 {
-                    return r;
+                    return item;
                 }
             }
 
@@ -55,9 +51,7 @@ namespace Automation_Example_App
         /// <returns>The web element that matches the search criteria or returns nothing.</returns>
         public static IWebElement GetElementByID(IWebDriver driver, string searchId, string searchText)
         {
-            var results = driver.FindElements(By.Id(searchId));
-
-            foreach (var r in results)
+            foreach (var r in driver.FindElements(By.Id(searchId)))
             {
                 if (r.Text == searchText)
                 {
