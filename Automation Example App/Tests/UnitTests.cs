06e62f6092d0d2ce6c4024ba354c379d2f364869
Haagimus <Haagimus@gmail.com>
2018-05-28 23:14:25 -0500

    removed unecessary using statements

--- Automation Example App/Tests/UnitTests.cs
+++ Automation Example App/Tests/UnitTests.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Automation_Example_App.Tests
 {
